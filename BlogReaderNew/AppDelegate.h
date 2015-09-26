b5dca39ffc7c7ef9cbffa705107478ec4630ea3c
Adam Goldberg <adammgoldberg@hotmail.com>
2015-09-26 16:54:43 -0400

    Initial Commit

---
+++ BlogReaderNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogReaderNew
+//
+//  Created by Adam Goldberg on 2015-09-26.
+//  Copyright (c) 2015 Adam Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
